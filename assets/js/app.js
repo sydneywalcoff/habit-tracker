f3f9cc1e68706b8ce351af3951ca2ab3deca5fc8
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-08-19 13:46:18 -0700

    greeting dynamically updates based on time of day

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,18 @@ const DateTime = luxon.DateTime;
 
 // grab currentDay
 const dt = DateTime.local();
-const weekDay = dt.weekdayLong;
-console.log(weekDay)
+const weekDay = dt.weekdayLong.toLowerCase();
 $("#currentDay").text(weekDay)
 
 
+// update greeting
+const currentHour = dt.hour;
+console.log(currentHour)
 
-// if()
-// $("#greeting")
-
+if(currentHour < 12) {
+    $("#greeting").text('good morning')
+} else if (12 <= currentHour < 18) {
+    $("#greeting").text('good afternoon')
+} else if (18 <= currentHour) {
+    $("#greeting").text('good evening')
+}
