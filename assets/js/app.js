0d9da20cfc46c57209358179dae79162100724eb
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-08-19 14:56:17 -0700

    dates dynamically update to show current week m-sun

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,7 @@ const DateTime = luxon.DateTime;
 // grab currentDay
 const dt = DateTime.local();
 const weekDay = dt.weekdayLong.toLowerCase();
-$("#currentDay").text(weekDay)
-
+$("#currentDay").text(weekDay);
 
 // update greeting
 const currentHour = dt.hour;
@@ -16,6 +15,28 @@ if(currentHour < 12) {
     $("#greeting").text('good afternoon')
 } else if (18 <= currentHour) {
     $("#greeting").text('good evening')
+}
+
+
+// add dates to tracker
+const dayOfWeek = dt.weekday;
+const month = dt.month;
+const day = dt.day;
+if(dayOfWeek === 1) {
+    $("#1").text(`${month}/${day}`)
+    $("#2").text(`${month}/${day+1}`)
+    $("#3").text(`${month}/${day+2}`)
+    $("#4").text(`${month}/${day+3}`)
+    $("#5").text(`${month}/${day+4}`)
+    $("#6").text(`${month}/${day+5}`)
+    $("#7").text(`${month}/${day+6}`)
+} else if (dayOfWeek !== 1) {
+    for(let i =1; i < 8; i++ ){
+        const difference = dayOfWeek - i;
+        $("#"+ i).text(`${month}/${day - difference}`)
+    }
+} else {
+    console.log('something is wrong :(')
 }
 
 // copywrite
