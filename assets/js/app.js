6aa03bdc1f0891876ae9359bd9c254f904ac145d
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-08-19 13:32:53 -0700

    day of week updates dynamically

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,14 @@
-// external imports
+// initialize luxon
+const DateTime = luxon.DateTime;
+
+// grab currentDay
+const dt = DateTime.local();
+const weekDay = dt.weekdayLong;
+console.log(weekDay)
+$("#currentDay").text(weekDay)
 
 
-// internal imports
 
+// if()
+// $("#greeting")
 
-// assets
