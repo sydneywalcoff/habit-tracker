6b987d451aaf16b71ac2fcf5be86b225fcd73677
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-10-21 15:42:12 -0700

    recentered tracker and added dates

--- client/src/components/Greeting/index.js
+++ client/src/components/Greeting/index.js
@@ -12,37 +12,16 @@ const Greeting = () => {
     // update greeting
     const currentHour = dt.hour;
     if (currentHour < 12) {
-        $("#greeting").text('good morning')
-    } else if (12 <= currentHour && currentHour< 18) {
-        $("#greeting").text('good afternoon')
+        $("#greeting").text('morning')
+    } else if (12 <= currentHour && currentHour < 18) {
+        $("#greeting").text('afternoon')
     } else if (18 <= currentHour) {
-        $("#greeting").text('good evening')
+        $("#greeting").text('evening')
     }
-
-    // add dates to tracker
-    const dayOfWeek = dt.weekday;
-    const month = dt.month;
-    const day = dt.day;
-    if (dayOfWeek === 1) {
-        $("#1").text(`${month}/${day}`)
-        $("#2").text(`${month}/${day + 1}`)
-        $("#3").text(`${month}/${day + 2}`)
-        $("#4").text(`${month}/${day + 3}`)
-        $("#5").text(`${month}/${day + 4}`)
-        $("#6").text(`${month}/${day + 5}`)
-        $("#7").text(`${month}/${day + 6}`)
-    } else if (dayOfWeek !== 1) {
-        for (let i = 1; i < 8; i++) {
-            const difference = dayOfWeek - i;
-            $("#" + i).text(`${month}/${day - difference}`)
-        }
-    } else {
-        console.log('something is wrong :(')
-    }
-    return (
+    return ( 
         <>
-            <h5 className="text-center"><span id="greeting"></span>, sydney!</h5>
-            <h6 className="text-center">today is <span id="currentDay"></span>.</h6>
+            <h5 className = "text-center">good <span id = "greeting"> </span>, sydney!</h5>
+            <h6 className = "text-center">today is <span id = "currentDay"></span>.</h6>
         </>
     );
 };
--- client/src/components/Tracker/index.js
+++ client/src/components/Tracker/index.js
@@ -1,40 +1,53 @@
 import React from 'react';
+import { Button } from 'react-bootstrap'
+import { DateTime } from 'luxon';
+
 
 const Tracker = () => {
     const daysOfTheWeek = ['Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
     const habits = ['Morning Routine', 'Exercise', 'Dog Training', 'Water', 'Cleaning', 'Music', 'Meditation', 'Languages', 'Reading', 'Night Routine'];
 
+    const add = () => {
+        console.log('hi')
+    };
 
-    return(
+    const dayOfWeek = DateTime.local().weekday;
+    const month = DateTime.local().month;
+    const date = DateTime.local().day;
+
+   
+
+    return (
         <>
             <table className="table">
                 <thead>
                     <tr>
                         <th scope='col'>Task:</th>
-                        {daysOfTheWeek.map(day =>
-                            <th scope='col' className="text-center">{day}: <span id="1"></span></th>
+                        {daysOfTheWeek.map((day, i) =>
+                            <th scope='col' className="text-center" key={i}>{day}: <span id={i+1}>{dayOfWeek === 1 ? `${month}/ ${date - i}` : `${month}/ ${date - dayOfWeek + 1 + i}`}</span></th>
                         )}
                     </tr>
                 </thead>
                 <tbody>
-                    {habits.map(habit =>
-                        <tr>
+                    {habits.map((habit, i) =>
+                        <tr key={i}>
                             <th scope="row">{habit}:</th>
-                            {daysOfTheWeek.map((day,i) =>
-                                <td className="text-center"><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" key ={i}/></td>
+                            {daysOfTheWeek.map((day, i) =>
+                                <td className="text-center" key={i}><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/></td>
                             )}
                         </tr>
-                    )}  
+                    )}
                 </tbody>
             </table>
             <div id="buttonDiv" className="d-flex justify-content-center my-4">
-                <button className="btn btn-outline-dark mx-1" id="add-btn">add.</button>
-                <button className="btn btn-outline-dark mx-1" id="edit-btn">edit.</button>
-                <button className="btn btn-outline-dark mx-1" id="delete-btn">delete.</button>
+                <Button variant="outline-dark" className="mx-1" id="add-btn" onClick={add}>add.</Button>
+                <Button variant="outline-dark" className="btn btn-outline-dark mx-1" id="edit-btn" >edit.</Button>
+                <Button variant="outline-dark" className="btn btn-outline-dark mx-1" id="delete-btn">delete.</Button>
             </div>
         </>
     );
 };
+
 
 
 export default Tracker;
