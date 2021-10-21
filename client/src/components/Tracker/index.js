f4d1139506986cad6c5d26c11a711fc9efcf6ce2
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-10-21 16:19:28 -0700

    add button now clears the buttonDiv

--- client/src/components/Tracker/index.js
+++ client/src/components/Tracker/index.js
@@ -1,21 +1,35 @@
 import React from 'react';
 import { Button } from 'react-bootstrap'
 import { DateTime } from 'luxon';
+import $ from 'jquery';
 
 
 const Tracker = () => {
     const daysOfTheWeek = ['Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
     const habits = ['Morning Routine', 'Exercise', 'Dog Training', 'Water', 'Cleaning', 'Music', 'Meditation', 'Languages', 'Reading', 'Night Routine'];
 
-    const add = () => {
-        console.log('hi')
+    const addButtons = () => {
+
+        const addButtonHandler = () => {
+            $('#buttonDiv').empty();
+            
+        };
+
+        return (
+            <>
+                <Button variant="outline-dark" className="mx-1" id="add-btn" onClick = {addButtonHandler}>add.</Button>
+                <Button variant="outline-dark" className="btn btn-outline-dark mx-1" id="edit-btn" >edit.</Button>
+                <Button variant="outline-dark" className="btn btn-outline-dark mx-1" id="delete-btn">delete.</Button>
+            </>
+        );
+
     };
 
     const dayOfWeek = DateTime.local().weekday;
     const month = DateTime.local().month;
     const date = DateTime.local().day;
 
-   
+
 
     return (
         <>
@@ -24,7 +38,7 @@ const Tracker = () => {
                     <tr>
                         <th scope='col'>Task:</th>
                         {daysOfTheWeek.map((day, i) =>
-                            <th scope='col' className="text-center" key={i}>{day}: <span id={i+1}>{dayOfWeek === 1 ? `${month}/ ${date - i}` : `${month}/ ${date - dayOfWeek + 1 + i}`}</span></th>
+                            <th scope='col' className="text-center" key={i}>{day}: <span id={i + 1}>{dayOfWeek === 1 ? `${month}/ ${date - i}` : `${month}/ ${date - dayOfWeek + 1 + i}`}</span></th>
                         )}
                     </tr>
                 </thead>
@@ -33,16 +47,14 @@ const Tracker = () => {
                         <tr key={i}>
                             <th scope="row">{habit}:</th>
                             {daysOfTheWeek.map((day, i) =>
-                                <td className="text-center" key={i}><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/></td>
+                                <td className="text-center" key={i}><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
                             )}
                         </tr>
                     )}
                 </tbody>
             </table>
             <div id="buttonDiv" className="d-flex justify-content-center my-4">
-                <Button variant="outline-dark" className="mx-1" id="add-btn" onClick={add}>add.</Button>
-                <Button variant="outline-dark" className="btn btn-outline-dark mx-1" id="edit-btn" >edit.</Button>
-                <Button variant="outline-dark" className="btn btn-outline-dark mx-1" id="delete-btn">delete.</Button>
+                {addButtons()}
             </div>
         </>
     );
