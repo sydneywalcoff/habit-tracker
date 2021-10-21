4da34d0cc3dae3520f815d4800168c85574651a6
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-10-21 15:46:38 -0700

    luxon working fully again

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { DateTime } from 'luxon'
 
 const Footer = () => {
+
     return (
         <footer className="mt-4">
-            <h6 className="text-center">sydney walcoff <span id="copywrite">{DateTime.year}</span></h6>
+            <h6 className="text-center">sydney walcoff <span id="copywrite">{DateTime.local().year}©</span></h6>
         </footer>
     );
 };
--- client/src/components/Greeting/index.js
+++ client/src/components/Greeting/index.js
@@ -8,20 +8,20 @@ const Greeting = () => {
     const dt = DateTime.local();
     const weekDay = dt.weekdayLong.toLowerCase();
     $("#currentDay").text(weekDay);
-
+    let greeting;
     // update greeting
     const currentHour = dt.hour;
     if (currentHour < 12) {
-        $("#greeting").text('morning')
+        greeting = 'morning'
     } else if (12 <= currentHour && currentHour < 18) {
-        $("#greeting").text('afternoon')
+        greeting = 'afternoon'
     } else if (18 <= currentHour) {
-        $("#greeting").text('evening')
+        greeting = 'evening'
     }
     return ( 
         <>
-            <h5 className = "text-center">good <span id = "greeting"> </span>, sydney!</h5>
-            <h6 className = "text-center">today is <span id = "currentDay"></span>.</h6>
+            <h5 className = "text-center">good <span id = "greeting">{greeting}</span>, sydney!</h5>
+            <h6 className = "text-center">today is <span id = "currentDay">{weekDay}</span>.</h6>
         </>
     );
 };
