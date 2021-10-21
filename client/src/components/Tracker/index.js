b41b19e5cb528b042afffbb0eebf20d3d5f415ed
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-10-21 14:01:47 -0700

    refactored tracker table further

--- client/src/components/Tracker/index.js
+++ client/src/components/Tracker/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const Tracker = () => {
     const daysOfTheWeek = ['Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const habits = ['Morning Routine', 'Exercise', 'Dog Training', 'Water', 'Cleaning', 'Music', 'Meditation', 'Languages', 'Reading', 'Night Routine'];
     return(
         <>
             <table className="table">
@@ -14,66 +15,14 @@ const Tracker = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">Morning Routine:</th>
-                        {daysOfTheWeek.map(day =>
-                            <td className="text-center"><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
-                        )}
-                    </tr>
-                    <tr>
-                        <th scope="row">Exercise:</th>
-                        {daysOfTheWeek.map(day =>
-                            <td className="text-center"><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
-                        )}
-                    </tr>
-                    <tr>
-                        <th scope="row">Dog Training:</th>
-                        {daysOfTheWeek.map(day =>
-                            <td className="text-center"><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
-                        )}
-                    </tr>
-                    <tr>
-                        <th scope="row">Water:</th>
-                        {daysOfTheWeek.map(day =>
-                            <td className="text-center"><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
-                        )}
-                    </tr>
-                    <tr>
-                        <th scope="row">Cleaning:</th>
-                        {daysOfTheWeek.map(day =>
-                            <td className="text-center"><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
-                        )}
-                    </tr>
-                    <tr>
-                        <th scope="row">Music:</th>
-                        {daysOfTheWeek.map(day =>
-                            <td className="text-center"><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
-                        )}
-                    </tr>
-                    <tr>
-                        <th scope="row">Meditation:</th>
-                        {daysOfTheWeek.map(day =>
-                            <td className="text-center"><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
-                        )}
-                    </tr>
-                    <tr>
-                        <th scope="row">Languages:</th>
-                        {daysOfTheWeek.map(day =>
-                            <td className="text-center"><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
-                        )}
-                    </tr>
-                    <tr>
-                        <th scope="row">Reading:</th>
-                        {daysOfTheWeek.map(day =>
-                            <td className="text-center"><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
-                        )}
-                    </tr>
-                    <tr>
-                        <th scope="row">Night Routine:</th>
-                        {daysOfTheWeek.map(day =>
-                            <td className="text-center"><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
-                        )}
-                    </tr>
+                    {habits.map(habit =>
+                        <tr>
+                            <th scope="row">{habit}</th>
+                            {daysOfTheWeek.map(day =>
+                                <td className="text-center"><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
+                            )}
+                        </tr>
+                    )}  
                 </tbody>
             </table>
             <div id="buttonDiv" className="d-flex justify-content-center my-4">
