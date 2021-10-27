5e5a776f9ca5b6eb8e670d485657ca940c4dcf11
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-10-27 14:04:15 -0700

    ui performs basic actions, but doesn't save edited habit

--- client/src/components/Tracker/index.js
+++ client/src/components/Tracker/index.js
@@ -5,52 +5,73 @@ import { DateTime } from 'luxon';
 
 const Tracker = () => {
     const [buttonState, setButtonState] = useState('');
-    const [value, setValueState] = useState('');
+    const [formValue, setFormValueState] = useState('');
+    const [formState, setFormState] = useState('');
     const daysOfTheWeek = ['Mon', 'Tues', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    const [habits, setHabitsState] = useState(['Morning Routine', 'Exercise', 'Dog Training', 'Water', 'Cleaning', 'Music', 'Meditation', 'Languages', 'Reading', 'Night Routine']);
+    const [habits] = useState(['Morning Routine', 'Exercise', 'Dog Training', 'Water', 'Cleaning', 'Music', 'Meditation', 'Languages', 'Reading', 'Night Routine']);
 
     const dayOfWeek = DateTime.local().weekday;
     const month = DateTime.local().month;
     const date = DateTime.local().day;
 
+    const handleChange = (e) => {
+        if (buttonState === 'add') {
+            setFormValueState(e.target.value)
+        }
+        if (buttonState === 'edit') {
+            const oldValue =
+                console.log(e.target.id)
+        }
+    };
+
     const buttonHandler = () => {
-        const handleChange = (e) => {
-            setValueState(e.target.value)
-        };
 
         const addButtonHandler = () => {
             setButtonState('add');
         };
 
-        const saveButtonHandler = () => {
-            habits.push(value)
+        const editButtonHandler = () => {
+            setButtonState('edit');
+            setFormState('edit');
+        };
 
+        const saveButtonHandler = () => {
+            if (buttonState === 'add') {
+                habits.push(formValue)
+            }
+            if (buttonState === 'edit') {
+                setFormState('');
+            }
             setButtonState('');
         };
+
 
         if (buttonState === 'add') {
             return (
                 <>
                     <Form>
                         <Form.Group>
-                            <Form.Control as="textarea" rows={2} onChange={handleChange}/>
+                            <Form.Control as="textarea" rows={2} onChange={handleChange} />
                         </Form.Group>
                     </Form>
                     <Button variant="outline-dark" className="mx-1" id="save-btn" onClick={saveButtonHandler}>save.</Button>
                 </>
             );
+        } else if (buttonState === 'edit') {
+            return (
+                <Button variant="outline-dark" className="mx-1" id="save-btn" onClick={saveButtonHandler}>save.</Button>
+            );
         }
+
         return (
             <>
                 <Button variant="outline-dark" className="mx-1" id="add-btn" onClick={addButtonHandler}>add.</Button>
-                <Button variant="outline-dark" className="btn btn-outline-dark mx-1" id="edit-btn" >edit.</Button>
+                <Button variant="outline-dark" className="btn btn-outline-dark mx-1" id="edit-btn" onClick={editButtonHandler}>edit.</Button>
                 <Button variant="outline-dark" className="btn btn-outline-dark mx-1" id="delete-btn">delete.</Button>
             </>
         );
 
     };
-
-
 
     return (
         <>
@@ -63,16 +84,35 @@ const Tracker = () => {
                         )}
                     </tr>
                 </thead>
-                <tbody>
-                    {habits.map((habit, i) =>
-                        <tr key={i}>
-                            <th scope="row">{habit}:</th>
-                            {daysOfTheWeek.map((day, i) =>
-                                <td className="text-center" key={i}><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
-                            )}
-                        </tr>
-                    )}
-                </tbody>
+                {formState ? (
+                    <tbody>
+                        {habits.map((habit, i) =>
+                            <tr key={i}>
+                                <th scope="row">
+                                    <Form id={i}>
+                                        <Form.Group id={i}>
+                                            <Form.Control type="text" placeholder={habit} onChange={handleChange} id={i} />
+                                        </Form.Group>
+                                    </Form>
+                                </th>
+                                {daysOfTheWeek.map((day, i) =>
+                                    <td className="text-center" key={i}><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
+                                )}
+                            </tr>
+                        )}
+                    </tbody>
+                ) : (
+                    <tbody>
+                        {habits.map((habit, i) =>
+                            <tr key={i}>
+                                <th scope="row">{habit}:</th>
+                                {daysOfTheWeek.map((day, i) =>
+                                    <td className="text-center" key={i}><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
+                                )}
+                            </tr>
+                        )}
+                    </tbody>
+                )}
             </table>
             <div id="buttonDiv" className="d-flex justify-content-center my-4">
                 {buttonHandler()}
