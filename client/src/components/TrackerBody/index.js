f2789e46990f934e2cb07e3d40236ee0fec3e775
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-11-03 15:33:39 -0700

    last fix on the delete button

--- client/src/components/TrackerBody/index.js
+++ client/src/components/TrackerBody/index.js
@@ -22,41 +22,33 @@ const TrackerBody = ({ formState, handleChange, habits, saveHabits, setHabitsSta
             </tbody>
         );
     }
+    const deleteHabitButton = (i) => {
+        if (formState === 'delete') {
+            const deleteHabit = (e) => {
+                const index = parseInt(e.target.attributes.i.textContent);
+                const tempArr = [...habits];
+                tempArr.splice(index, 1);
+                setHabitsState(tempArr);
+                saveHabits(tempArr);
+                if (tempArr.length === 0) {
+                    setButtonState('add')
+                }
+            };
+            return (
+                <Button variant="outline-dark" className="btn btn-outline-dark" onClick={deleteHabit} i={i}>delete.</Button>
+            );
+        }
+    };
 
-    if (formState === 'delete') {
-        const deleteHabit = (e) => {
-            const index = parseInt(e.target.attributes.i.textContent);
-            const tempArr = [...habits];
-            tempArr.splice(index, 1);
-            setHabitsState(tempArr);
-            saveHabits(tempArr);
-            if (tempArr.length === 0) {
-                setButtonState('add')
-            }
-        };
-        return (
-            <tbody>
-                {habits.map((habit, i) =>
-                    <tr key={i}>
-                        <th scope="row" className="d-flex justify-content-between">
-                            {habit}:
-                            <Button variant="outline-dark" className="btn btn-outline-dark" onClick={deleteHabit} i={i}>delete.</Button>
-                        </th>
-
-                        {daysOfTheWeek.map((day, i) =>
-                            <td className="text-center" key={i}><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
-                        )}
-                    </tr>
-                )}
-            </tbody>
-        );
-    }
 
     return (
         <tbody>
             {habits.map((habit, i) =>
                 <tr key={i}>
-                    <th scope="row">{habit}:</th>
+                    <th scope="row" className="d-flex justify-content-between">
+                        {habit}:
+                        {deleteHabitButton(i)}
+                    </th>
                     {daysOfTheWeek.map((day, i) =>
                         <td className="text-center" key={i}><input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" /></td>
                     )}
