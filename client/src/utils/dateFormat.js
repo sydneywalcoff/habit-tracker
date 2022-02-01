cd02a987236598f50cbe83ff48fd92081e5defbf
Sydney Walcoff <sydney.walcoff@gmail.com>
2022-02-01 11:09:03 -0800

    dates now accomodate for new year and new months that don't line up with the new week; needs refactoring

--- client/src/utils/dateFormat.js
+++ client/src/utils/dateFormat.js
@@ -1,14 +1,47 @@
 import { DateTime } from 'luxon';
 
 const dayOfWeek = DateTime.local().weekday;
-const month = DateTime.local().month;
-const date = DateTime.local().day;
+// const month = DateTime.local().month;
+// const date = DateTime.local().day;
+let month = 3;
+const date = 29;
 const weekNumber = DateTime.local().weekNumber;
 const year = DateTime.local().year;
 
+const months = {
+    '1': '31',
+    '2': '28',
+    '3': '31',
+    '4': '30',
+    '5': '31',
+    '6': '30',
+    '7': '31',
+    '8': '31',
+    '9': '30',
+    '10': '31',
+    '11': '30',
+    '12': '31'
+}
+
 const formatDate = (i) => {
     let formattedDate;
-    dayOfWeek === 1 ? formattedDate = `${month}/${date - i}` : formattedDate =`${month}/${date - dayOfWeek + 1 + i}`
+    if(dayOfWeek === 1) {
+        formattedDate = `${month}/${date - i}`
+    } else {
+        if((date - dayOfWeek + 1 + i) === 0) {
+            formattedDate = `${month -1 }/${months[month-1]}`
+        } else if ((date - dayOfWeek + 1 + i) > months[month]) {
+            // accomodates for new year
+            if(month+1 > 12)  {
+                formattedDate = `1/${date - dayOfWeek + 1 + i - parseInt(months[12])}`
+            } else {
+                formattedDate = `${month+1}/${date - dayOfWeek + 1 + i - parseInt(months[12])}`
+            }
+            
+        } else {
+            formattedDate =`${month}/${date - dayOfWeek + 1 + i}`
+        }
+    }
     
     return formattedDate;
 };
